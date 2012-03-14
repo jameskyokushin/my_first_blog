5dad81cb46187d82969510d438caa4788ad54f8a
jameskyokushin <james@kyokushin.com.ph>
2012-03-14 14:55:39 +0800

    blog working must insert design for better viewing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '93e41adafea9775528477626429ad56d95d15812eeaaa5c6e1f469ae0ed4a40c80c6db7f1940c2a04f0704d5d77db2a77169caabe423187de5d8a7cd0b524a3a'
